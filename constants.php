71fe0025f2db35a558c3d1e6fd814cb01e08939b
Dylan McKee <dylan@djmckee.co.uk>
2016-02-29 18:21:48 +0000

    Added Web API

---
+++ constants.php
@@ -0,0 +1,17 @@
+<?php
+    /*
+        A PHP file to hold constants that are shared between the other PHP files
+        within the Project Crescendo API - i.e. database logins.
+
+        Author: Dylan McKee
+        Date: 28/02/2016
+
+    */
+
+    // I looked up the use of the define() function at https://secure.php.net/manual/en/function.define.php
+    define("DATABASE_HOST", "homepages.cs.ncl.ac.uk");
+    define("DATABASE_NAME", "t2022t13");
+    define("DATABASE_USER", "t2022t13");
+    define("DATABASE_PASSWORD", "Aye,.6Bums");
+
+?>
