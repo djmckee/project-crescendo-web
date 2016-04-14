a56dd445e52f8bd282993dee3f084b10f998c872
Dylan McKee <dylan@djmckee.co.uk>
2016-04-14 15:01:08 +0100

    Added Client ID and Client Secret authentication

--- constants.php
+++ constants.php
@@ -1,7 +1,7 @@
 <?php
     /*
         A PHP file to hold constants that are shared between the other PHP files
-        within the Project Crescendo API - i.e. database logins.
+        within the Project Crescendo API - i.e. database logins and client IDs.
 
         Author: Dylan McKee
         Date: 28/02/2016
@@ -13,5 +13,7 @@
     define("DATABASE_NAME", "t2022t13");
     define("DATABASE_USER", "t2022t13");
     define("DATABASE_PASSWORD", "Aye,.6Bums");
+    define("CLIENT_ID", "VJyR29hQKnwG3R8CjbN6");
+    define("CLIENT_SECRET", "HgNzSKYJqQKyNT2mGVSUr4cUvMcGs2Ym7x3U5dHA");
 
 ?>
