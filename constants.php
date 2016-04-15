ce23239e261cbb8d0ffa363bef7b280c55da209a
Dylan McKee <dylan@djmckee.co.uk>
2016-04-15 10:57:06 +0100

    Revised documentation

--- constants.php
+++ constants.php
@@ -1,19 +1,56 @@
 <?php
-    /*
-        A PHP file to hold constants that are shared between the other PHP files
-        within the Project Crescendo API - i.e. database logins and client IDs.
+    /**
+     *  A PHP file to hold constants that are shared between the other PHP files
+     *  within the Project Crescendo API - i.e. database logins and client IDs.
+     *
+     *  I looked up the use of the define() function at
+     *  https://secure.php.net/manual/en/function.define.php
+     *
+     *  Author: Dylan McKee
+     *  Date: 28/02/2016
+     *
+     */
 
-        Author: Dylan McKee
-        Date: 28/02/2016
-
-    */
-
-    // I looked up the use of the define() function at https://secure.php.net/manual/en/function.define.php
+    /**
+     * The MySQL Database Host Address
+     */
     define("DATABASE_HOST", "homepages.cs.ncl.ac.uk");
+
+    /**
+     * The MySQL Database name
+     */
     define("DATABASE_NAME", "***REMOVED***");
+
+    /**
+     * The MySQL Database Username
+     */
     define("DATABASE_USER", "***REMOVED***");
+
+    /**
+     * The super secret MySQL Database Password
+     */
     define("DATABASE_PASSWORD", "***REMOVED***");
+
+    /**
+     * The client ID - this value must be sent by the client when interacting
+     * with this API, as a 'client_id' value in the JSON body posted to this API.
+     *
+     * If we ever develop the mobile client app on another platform, this value
+     * will have to be stored in the database and associted with the relevant
+     * secret for that particular app, but for now, it's fine to just define
+     * this as a one-off constant.
+     */
     define("CLIENT_ID", "VJyR29hQKnwG3R8CjbN6");
+
+    /**
+     * The client secret - this value must be sent by the client when interacting
+     * with this API, as a 'client_secret' value in the JSON body posted to this API.
+     *
+     * If we ever develop the mobile client app on another platform, this value
+     * will have to be stored in the database and associted with the relevant
+     * id for that particular app, but for now, it's fine to just define
+     * this as a one-off constant.
+     */
     define("CLIENT_SECRET", "HgNzSKYJqQKyNT2mGVSUr4cUvMcGs2Ym7x3U5dHA");
 
 ?>
